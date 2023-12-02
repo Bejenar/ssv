7f71b6d38e261a83bdccc6ef1487af6f1996fe4c
Bejenar Nicolai <admynyral2808@gmail.com>
2023-12-02 12:16:41 +0200

    at1

---
+++ Assets/Spike.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spike : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Spike enter, losing level ");
+            SceneManager.LoadScene("Lose");
+        }
+    }
+}
